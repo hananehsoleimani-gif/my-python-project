041b2ea6173298b857feb3d4d5ceb23c91958fe7
Hananeh Soleimani <hananehsleimani815@gmail.com>
2025-12-14 09:17:00 +0330

    add multiple practice

---
+++ try3.py
@@ -0,0 +1,52 @@
+"""radius =int(input("enter the radius :"))
+Area = radius*radius*3.14
+print(Area)"""
+
+
+
+"""num1 = int(input("enter the first number :"))
+num2 = int(input("enter the second number :"))
+num3 = int(input("enter the third number :"))
+avg = (num1+num2+num3)/3
+print(avg)"""
+
+#for i in range(1,4):  num= int(input("enter the first number :"))
+
+"""number =1%2
+print(number)"""
+
+"""purchaseAmount = eval(input("enter the purchase amount :"))
+Tax = purchaseAmount * 0.6
+print("sales tax is" , (Tax*100)/100.)"""
+
+"""value = 4.6
+print(int(value))
+print(round(value))
+print(eval("4 * 5 + 2"))
+print(int("04 "))
+print(int("4 "))
+print(eval("4 "))"""
+
+"""import time
+current_time = time.time()
+print(current_time)
+total_seconds = int(current_time)
+total_minutes = total_seconds // 60
+print(total_minutes)"""
+#ComputeLoan
+"""annualInterestRate = eval(input("enter the annual interest rate :"))
+monthlyInterestRate = annualInterestRate /1200
+numbersOfYears = eval(input("enter the number of years :"))
+loanAmount = eval(input("enter the loan amount :"))
+monthlyPayment = loanAmount*monthlyInterestRate/(1-1/(1+monthlyInterestRate)**(numbersOfYears*12))
+totalPayment = monthlyPayment * numbersOfYears*12
+
+print("The total payment is", int(totalPayment*100)/100)
+print("The monthly payment is" ,int( monthlyPayment*100 )/100)"""
+#ComputeDistance
+"""x1,y1 = eval(input("enter the x1 AND y1 :"))
+x2,y2 = eval(input("enter the x2 AND y2 :"))
+distance = ((x2-x1)**2 + (y2-y1)**2)**0.5
+print(f"The distance is {distance}")"""
+
+
